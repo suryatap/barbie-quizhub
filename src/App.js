99ec1a305cb3c1752c925edf2af5fd8245303b7a
unknown <suryatapadebnath559@gmail.com>
2021-07-22 12:23:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,54 +1,53 @@
 import axios from "axios";
-import { BrowserRouter ,Switch, Route} from 'react-router-dom';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Home from "./Pages/Home/Home"; 
-import Quiz from  './Pages/Quiz/Quiz';
+import Home from "./Pages/Home/Home";
+import Quiz from "./Pages/Quiz/Quiz";
 import Result from "./Pages/Result/Result";
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
- const[name,setName] =useState('');
- const [questions, setQuestions] = useState();
- const [score, setScore] = useState(0);
+  const [name, setName] = useState("");
+  const [questions, setQuestions] = useState();
+  const [score, setScore] = useState(0);
 
- const fetchQuestions=async(category="",difficulty="")=>{
-   const {data}=await axios.get( `https://opentdb.com/api.php?amount=10${
-    category && `&category=${category}`
-  }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
-);
-setQuestions(data.results);
-
- }
+  const fetchQuestions = async (category = "", difficulty = "") => {
+    const { data } = await axios.get(
+      `https://opentdb.com/api.php?amount=10${
+        category && `&category=${category}`
+      }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+    );
+    setQuestions(data.results);
+  };
   return (
     <BrowserRouter>
-    <div className="app" style={{backgroundImage:"url(./ques1.png)"}}>
-      <Header/>
-<Switch>
-  <Route path='/' exact>
-    <Home name={name}
-     setName={setName}
-    fetchQuestions={fetchQuestions}/> 
-  </Route >
-  <Route path='/quiz' exact>
-    <Quiz 
-    name={name}
-    questions={questions}
-    score={score}
-    setScore={setScore}
-    setQuestions={setQuestions}/> 
-  </Route >
-  <Route path='/result' exact>
-    <Result name={name} score={score}/> 
-  </Route >
-
-</Switch>
-
-            
-    </div>
-    <Footer />
-
+      <div className="app" style={{ backgroundImage: "url(./ques1.png)" }}>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Home
+              name={name}
+              setName={setName}
+              fetchQuestions={fetchQuestions}
+            />
+          </Route>
+          <Route path="/quiz" exact>
+            <Quiz
+              name={name}
+              questions={questions}
+              score={score}
+              setScore={setScore}
+              setQuestions={setQuestions}
+            />
+          </Route>
+          <Route path="/result" exact>
+            <Result name={name} score={score} />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
     </BrowserRouter>
   );
 }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,59 +1,55 @@
-
-import './Home.css';
-import {Button, MenuItem, TextField} from "@material-ui/core";
-import Categories  from "../../Data/Categories";
+import "./Home.css";
+import { Button, MenuItem, TextField } from "@material-ui/core";
+import Categories from "../../Data/Categories";
 import { useState } from "react";
 import { useHistory } from "react-router";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
-const Home =({name,setName,fetchQuestions})=>{
-    const [category, setCategory] = useState("");
-    const [difficulty, setDifficulty] = useState("");
-    const [error, setError] = useState(false);
-    const history = useHistory();
-    const handleSubmit = () => {
-        if (!category || !difficulty || !name) {
-            setError(true);
-            return;
-          } else {
-            setError(false);
-            fetchQuestions(category, difficulty);
-            history.push('/quiz');
-          }
-        };
-    
+const Home = ({ name, setName, fetchQuestions }) => {
+  const [category, setCategory] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  const [error, setError] = useState(false);
+  const history = useHistory();
+  const handleSubmit = () => {
+    if (!category || !difficulty || !name) {
+      setError(true);
+      return;
+    } else {
+      setError(false);
+      fetchQuestions(category, difficulty);
+      history.push("/quiz");
+    }
+  };
 
-    return (
-    <div className='content'>
-        <div className='settings'>
-            <span style ={{fontSize:30}}>
-                Quiz Settings
-            </span>
-            <div className='settings_select'>
-            {error && <ErrorMessage>Please Fill all the fields</ErrorMessage>}
-                <TextField style={{marginBottom:25}} 
-                label ='Enter your Name'
-                 variant='outlined'
-                 onChange={(e) => setName(e.target.value)}
-                  />
-                 <TextField
-                 select 
-                 label='Select Category'
-                 variant='outlined' 
-                 style={{marginBottom:30}}
-                 onChange={(e) => setCategory(e.target.value)}
-                 value={category}
-                 >
-                     {
-                     Categories.map((cat)=>(
-                     <MenuItem key={cat.category} value={cat.value} >{cat.category}
-
-                     </MenuItem>
-                     ))}
-                   </TextField>
-                   <TextField
+  return (
+    <div className="content">
+      <div className="settings">
+        <span style={{ fontSize: 30 }}>Quiz Settings</span>
+        <div className="settings_select">
+          {error && <ErrorMessage>Please Fill all the fields</ErrorMessage>}
+          <TextField
+            style={{ marginBottom: 25 }}
+            label="Enter your Name"
+            variant="outlined"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            select
+            label="Select Category"
+            variant="outlined"
+            style={{ marginBottom: 30 }}
+            onChange={(e) => setCategory(e.target.value)}
+            value={category}
+          >
+            {Categories.map((cat) => (
+              <MenuItem key={cat.category} value={cat.value}>
+                {cat.category}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
             select
             label="Select Difficulty"
-             variant="outlined"
+            variant="outlined"
             style={{ marginBottom: 30 }}
             onChange={(e) => setDifficulty(e.target.value)}
             value={difficulty}
@@ -68,19 +64,19 @@ const Home =({name,setName,fetchQuestions})=>{
               Hard
             </MenuItem>
           </TextField>
-          <Button variant="contained"
+          <Button
+            variant="contained"
             color="primary"
-            size="large" onClick={handleSubmit}
-            >
-              Start Quiz
+            size="large"
+            onClick={handleSubmit}
+          >
+            Start Quiz
           </Button>
-
-            </div>
         </div>
-<img src='/r.svg' className='banner' alt="2 img"/>
-
+      </div>
+      <img src="/quiz.svg" className="banner" alt="quiz app" />
     </div>
-    )
-                     }                  
+  );
+};
 
 export default Home;
